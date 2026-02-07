1c75fe34546a99b933f14f3d30b633fc923350f3
Blirex <vanazubarev883@gmail.com>
2026-02-07 15:56:55 +0000

    Homework

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,29 @@
 package domain;
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+
+    public static int Calculate() {
+        int count = 0;
+
+        // Перебираємо всі 6-значні номери
+        for (int ticket = 0; ticket <= 999999; ticket++) {
+            int firstHalf = ticket / 1000; // перші три цифри
+            int secondHalf = ticket % 1000; // останні три цифри
+
+            if (sumOfDigits(firstHalf) == sumOfDigits(secondHalf)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    // Приватний метод для підрахунку суми цифр трьохзначного числа
+    private static int sumOfDigits(int num) {
+        int sum = 0;
+        sum += num / 100;         // сотні
+        sum += (num / 10) % 10;   // десятки
+        sum += num % 10;          // одиниці
+        return sum;
     }
 }
